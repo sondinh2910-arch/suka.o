f70ca11aebdb2590675a416ef8dca414a94ff3eb
sondinh2910-arch <sondinh2910@gmail.com>
2025-11-24 11:58:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ genai.configure(api_key=API_KEY)
 chat_session = None
 
 try:
-    model = genai.GenerativeModel("gemini-1.5-pro") 
+    model = genai.GenerativeModel("gemini-2.5") 
     chat_session = model.start_chat(history=[])
     print("ok.")
 except Exception as e:
