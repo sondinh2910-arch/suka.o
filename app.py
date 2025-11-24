3434fbc01111ca8cc0979fa20b263d373f35215e
sondinh2910-arch <sondinh2910@gmail.com>
2025-11-24 12:18:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def ask():
 #PHẦN 4: KHỞI ĐỘNG SERVER ---
 if __name__ == "__main__":
     print("silad____+-*/>>>>>>")
-    app.run(debug=True,host='0.0.0.0',port=2910)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
